68439df21c7d38c4634efd3801b684bf3cf0c181
nrtecno <trymailnr@gmail.com>
2026-09-13 11:35:21 +0530

    Update __init__.py

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,1 +1,8 @@
-from .helpers import generate_id, is_valid_url
+from .storage import (
+    user_data,
+    link_cache,
+    victim_data_store,
+    active_users,
+    save_active_users,
+    load_active_users
+)
