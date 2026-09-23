278b873febd95de9d1a1b096a6ca94ad02816018
nrtecno <trymailnr@gmail.com>
2026-09-23 10:28:55 +0530

    Import golive button handlers in __init__.py

--- bot/buttons/__init__.py
+++ bot/buttons/__init__.py
@@ -5,4 +5,5 @@ from .twit import handle_twit_button, handle_twit_callback
 from .snap import handle_snap_button, handle_snap_callback
 from .gmail import handle_gmail_button, handle_gmail_callback
 from .free import handle_freefire_button, handle_freefire_callback
-from .all_links import handle_all_links, handle_copy_all  # <-- ADD
+from .golive import handle_golive_button, handle_golive_callback
+from .all_links import handle_all_links, handle_copy_all
